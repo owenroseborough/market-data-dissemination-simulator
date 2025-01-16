734d8c6db603bfff1443c3480caf1af655d5ef35
OWEN ROSEBOROUGH <76256902+owenroseborough@users.noreply.github.com>
2025-01-15 20:38:22 -0500

    Add files via upload

---
+++ Server/common_includes.h
@@ -0,0 +1,25 @@
+#ifndef COMMON_INCLUDES_H
+#define COMMON_INCLUDES_H
+
+#include <iostream>
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <limits>
+#include <string>
+#include <vector>
+#include <numeric>
+#include <algorithm>
+#include <unordered_map>
+#include <memory>
+#include <variant>
+#include <optional>
+#include <tuple>
+#include <format>
+
+#endif
