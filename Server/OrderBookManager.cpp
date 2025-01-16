5b544692f93e301ef32059693397d58a5ccf0b8a
OWEN ROSEBOROUGH <76256902+owenroseborough@users.noreply.github.com>
2025-01-15 21:13:31 -0500

    Add files via upload

--- Server/OrderBookManager.cpp
+++ Server/OrderBookManager.cpp
@@ -10,12 +10,32 @@ using Symbol = string;
 
 class OrderBookManager {
 private:
-	unordered_map<Symbol, OrderBook> orderBookMap;
-	unordered_map<Symbol, size_t>    orderBookDepth;   // how many levels on bids/asks to deseminate to client
+	unordered_map<Symbol, shared_ptr<OrderBook>> orderBookMap;
+	unordered_map<Symbol, shared_ptr<size_t>>    orderBookDepth;   // how many levels on bids/asks to desseminate to client
 public:
-	OrderBookManager() {}
-	bool AddSymbol(Symbol, size_t) {
-		OrderBook 
+	bool AddSymbol(Symbol symbol, size_t depth) {
+
+		auto result = orderBookMap.insert({ symbol, make_shared<OrderBook>() });
+		orderBookDepth.insert({ symbol, make_shared<size_t>(depth) });
+		if (result.second) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	bool RemoveSymbol(Symbol symbol) {
+
+		if (orderBookMap.contains(symbol)) {
+			orderBookMap.erase(symbol);
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	shared_ptr<OrderBook> GetOrderBook(Symbol) {
+
 	}
 };
 
